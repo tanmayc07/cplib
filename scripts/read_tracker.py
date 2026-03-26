719efdb23f997ccf260533508d025885eb2210cd
Tanmay Chaudhari <hctanmay@gmail.com>
2026-03-26 10:35:19 -0400

    udpate script

--- scripts/read_tracker.py
+++ scripts/read_tracker.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from datetime import datetime
+import argparse
 
 def read_tracker(fpath):
     if not fpath.exists():
@@ -10,6 +11,9 @@ def read_tracker(fpath):
     
 if __name__ == "__main__":
     path = Path("tracker.md")
+    parser = argparse.ArgumentParser('read-tracker')
+    parser.add_argument('-n', '--note', action='store_true')
+    args = parser.parse_args()
     
     data = read_tracker(path)
     if data:
@@ -19,8 +23,12 @@ if __name__ == "__main__":
                 split_line = line.split('|')
                 print(
                     f"{split_line[2].strip()}_{split_line[4].strip()} {split_line[3].strip()}\n"
-                    f"First Solved: {split_line[1]}\n"
-                    f"Note:\n"
-                    f"{split_line[5].strip()}\n"
+                    f"First Solved: {split_line[1]}"
                 )
+                if args.note:
+                    print(
+                        f"Note:\n"
+                        f"{split_line[5].strip()}\n"
+                    )
+                print()
     
